e26916ae3681900159feb90c42502c6a5d97a1b4
Admin <admin@AbrahamdeMac-mini.local>
2021-08-09 20:24:58 +0800

    perfect mergeSort

--- src/algorithm/Sort/merge.js
+++ src/algorithm/Sort/merge.js
@@ -2,32 +2,26 @@
  * 归并排序
  * 先分后排，后序
  */
-function mergeSort (data = []) {
-    return recursion(0, data.length - 1)
-    function recursion (start, end) {
-        if (end <= start) return data.slice(start, end)
-        let middle = (end - start) / 2 | 0
-        let leftArr = recursion(start, middle)
-        let rightArr = recursion(middle, end)
+function mergeSort(data = []) {
+    return recursion(data)
+
+    function recursion(data = []) {
+        if (data.length < 2) return data
+        let middle = data.length / 2 | 0
+        let leftArr = recursion(data.slice(0, middle))
+        let rightArr = recursion(data.slice(middle))
         return merge(leftArr, rightArr)
     }
-    function merge (leftArr = [], rightArr = []) {
-        let length = leftArr.length + rightArr.length;
+    function merge(leftArr = [], rightArr = []) {
         let result = [];
-        let l = 0;
-        let r = 0;
-        for (let i = 0; i < length; i++) {
-            if (l >= leftArr.length) {
-                result[i] = rightArr[r++];
-            } else if (r >= rightArr.length) {
-                result[i] = leftArr[l++];
-            } else if (leftArr[l] > rightArr[r]) {
-                result[i] = rightArr[r++];
+        while (leftArr.length && rightArr.length) {
+            if (leftArr[0] < rightArr[0]) {
+                result.push(leftArr.shift())
             } else {
-                result[i] = leftArr[l++]
+                result.push(rightArr.shift())
             }
         }
-        return result
+        return leftArr.length ? result.concat(leftArr) : result.concat(rightArr)
     }
 }
 
